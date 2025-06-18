adb6d0a47e372524b918259cdd64fef1473632ef
belton mmata <beltonmmata@gmail.com>
2025-06-18 10:58:10 +0300

    url fixes

--- frontend/src/models/backendUrl.js
+++ frontend/src/models/backendUrl.js
@@ -1,3 +1,4 @@
 //const backendUrl = "http://localhost:5000/api/v1";
-const backendUrl = "https://azula-api.onrender.com/api/v1";
+//const backendUrl = "https://azula-api.onrender.com/api/v1";
+const backendUrl = import.meta.env.BACKEND_URI;
 export default backendUrl;
